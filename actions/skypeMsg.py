a3720306c957ffb480891e9b8e25f6994a2c2811
Parvin Hassan <parvinh@nihilentanalytics.com>
2019-09-25 18:45:21 +0530

    first commit

--- actions/skypeMsg.py
+++ actions/skypeMsg.py
@@ -5,8 +5,8 @@ from getpass import getpass # You probably don't want your password shown in pla
 
 class MyAction(Action):
 
-    def run(self, userName,password,contactName,message):
-        sk = Skype("parvin05.hassan@gmail.com", "Deca_135")
+    def run(self,userName,passwd,contactName,message):
+        sk = Skype(userName, passwd)
         ch = sk.contacts[contactName].chat
         ch.sendMsg(message) 
         print("message sent!!")
