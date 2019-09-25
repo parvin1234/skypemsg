78793052ffd8a4a8472b65b1ea8658709d36a43b
Parvin Hassan <parvinh@nihilentanalytics.com>
2019-09-25 18:49:15 +0530

    first commit

--- actions/skypeMsg.py
+++ actions/skypeMsg.py
@@ -1,12 +1,12 @@
 import sys
 from st2common.runners.base_action import Action
 from skpy import Skype
-from getpass import getpass # You probably don't want your password shown in plain text!
+
 
 class MyAction(Action):
 
-    def run(self,userName,passwd,contactName,message):
-        sk = Skype(userName, passwd)
+    def run(self,userName,password,contactName,message):
+        sk = Skype(userName, password)
         ch = sk.contacts[contactName].chat
         ch.sendMsg(message) 
         print("message sent!!")
